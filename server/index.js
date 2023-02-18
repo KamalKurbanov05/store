5320d0541ca77497e768b1a132c835d4eacc3476
Курбанов Камал <kamal8kamal@mail.ru>
2023-02-18 22:58:48 +0300

    add server and orm

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+const sequelize = require('./db');
+
+
+const app = express();
+
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync()
+        app.listen(PORT, () => console.log('port => :', PORT));
+    } catch(e) {
+        console.log('errr->',e)
+    }
+}
+
+start();
